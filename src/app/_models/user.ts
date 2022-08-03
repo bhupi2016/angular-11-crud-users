dc044557cfa61d087614ec984493bee60832bcf5
bhupi2016 <bhupi_jiet@yahoo.co.in>
2022-08-04 00:34:11 +0530

    1 file modified

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -2,10 +2,7 @@ import { Role } from './role';
 
 export class User {
     id!: string;
-    title!: string;
     firstName!: string;
     lastName!: string;
-    email!: string;
-    role!: Role;
     isDeleting: boolean = false;
 }
